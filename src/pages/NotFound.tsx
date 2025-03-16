638ab629dd8fa03010508212ea39bfecb905e730
Lucas Marques <marquesfaria99@gmail.com>
2025-03-16 12:53:09 -0300

    refact

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -6,7 +6,7 @@ const NotFound = () => {
 
   useEffect(() => {
     console.error(
-      "404 Error: User attempted to access non-existent route:",
+      "Erro 404: O usuário tentou acessar uma rota inexistente:",
       location.pathname
     );
   }, [location.pathname]);
@@ -17,7 +17,7 @@ const NotFound = () => {
         <h1 className="text-4xl font-bold mb-4">404</h1>
         <p className="text-xl text-gray-600 mb-4">Oops! Page not found</p>
         <a href="/" className="text-blue-500 hover:text-blue-700 underline">
-          Return to Home
+        Voltar para a página inicial
         </a>
       </div>
     </div>
